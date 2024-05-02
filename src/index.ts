a6303636e5afcaade9410f69339107477de2adfe
jwhowes <jacob.w.howes@durham.ac.uk>
2024-05-02 18:53:08 +0100

    Added constant line spacing (with leading and descending factored in)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {PDFDocument, StandardFonts, rgb} from 'pdf-lib';
+import {PDFDocument, PDFFont, StandardFonts, rgb} from 'pdf-lib';
 
 async function readTxt(e: any): Promise<String>{
 	const file = e.target.files[0];
@@ -9,59 +9,61 @@ async function readTxt(e: any): Promise<String>{
 	return file.text();
 }
 
-function pageSet(content: string, width: number, height: number, padding: number, font: any, fontSize: number): Array<string>{
-	const lineHeight: number = 2 * font.heightAtSize(fontSize);
-	const hPadding: number = 3 * lineHeight;
+function pageSet(content: string, width: number, height: number, yPadding: number, xPadding: number, font: PDFFont, fontSize: number, lineHeight: number): Array<Array<string>>{
+	let pages: Array<Array<string>> = [[]];
+	let currHeight: number = yPadding;
+	let currPage: number = 0;
 
-	let pages: Array<string> = [""];
-	let currHeight: number = hPadding;
-	console.log(hPadding);
-	console.log(lineHeight);
-
-	content.split("\r\n\r\n").forEach((paragraph, pidx) => {
-		let currWidth: number = padding;
-		let lines: Array<string> = [""];
-
-		paragraph.split(" ").forEach((word, widx) => {
+	content.split("\r\n\r\n").forEach((paragraph, _) => {
+		let currWidth: number = xPadding;
+		pages[currPage].push("");
+		paragraph.split(" ").forEach((word, _) => {
 			let wordWidth: number = font.widthOfTextAtSize(word + " ", fontSize);
-			if(wordWidth + currWidth >= width - padding){
-				lines.push("");
-				currWidth = padding;
+			if(wordWidth + currWidth >= width - xPadding){
+				if(currHeight + lineHeight >= height - yPadding){
+					pages.push([]);
+					currPage++;
+					currHeight = yPadding;
+				}
+				currHeight += lineHeight;
+				pages[currPage].push("");
+				currWidth = xPadding;
 			}
-			lines[lines.length - 1] += word + " ";
+
+			pages[currPage][pages[currPage].length - 1] += word + " ";
 			currWidth += wordWidth;
 		});
-
-		lines.forEach((line, lidx) => {
-			if(currHeight + lineHeight >= height - 2 * hPadding){
-				pages.push("");
-				currHeight = hPadding;
-			}
-			pages[pages.length - 1] += line + "\n";
-			currHeight += lineHeight;
-		});
-		pages[pages.length - 1] += "\n";
-		currHeight += lineHeight;
+		pages[currPage].push("");
+		currHeight += 2 * lineHeight;
 	});
 
 	return pages;
 }
 
 async function toPdf(content: string): Promise<void>{
-	const pdfDoc = await PDFDocument.create();
+	const pdfDoc: PDFDocument = await PDFDocument.create();
 
-	const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-	const fontSize = 11;
+	const font: PDFFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+	const fontSize: number = 11;
 	const [width, height] = [595.28, 841.89];
 
-	let pages: Array<string> = pageSet(content, width, height, 50, font, fontSize);
-	pages.forEach((pageText, idx) => {
+	const leading: number = 6;
+	const lineHeight = font.heightAtSize(fontSize) + leading;
+	const yPadding = 4 * fontSize;
+
+	let pages: Array<Array<string>> = pageSet(content, width, height, yPadding, 50, font, fontSize, lineHeight);
+
+	console.log(pages);
+
+	pages.forEach((lines, _) => {
 		let page = pdfDoc.addPage();
-		page.drawText(pageText, {
-			x: 50,
-			y: height - 4 * fontSize,
-			size: fontSize,
-			font: font
+		lines.forEach((line, idx) => {
+			page.drawText(line, {
+				x: 50,
+				y: height - yPadding - idx * lineHeight,
+				size: fontSize,
+				font: font
+			});
 		});
 	});
 
