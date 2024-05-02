a3a035b2e0f19572da17822644a96cc0dc847198
jwhowes <jacob.w.howes@durham.ac.uk>
2024-05-02 17:11:29 +0100

    Added greedy page breaks

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,19 @@ async function readTxt(e: any): Promise<String>{
 	return file.text();
 }
 
-function pageSet(content: string, width: number, padding: number, font: any, fontSize: number): string{
-	let ret = "";
+function pageSet(content: string, width: number, height: number, padding: number, font: any, fontSize: number): Array<string>{
+	const lineHeight: number = 2 * font.heightAtSize(fontSize);
+	const hPadding: number = 3 * lineHeight;
+
+	let pages: Array<string> = [""];
+	let currHeight: number = hPadding;
+	console.log(hPadding);
+	console.log(lineHeight);
 
 	content.split("\r\n\r\n").forEach((paragraph, pidx) => {
 		let currWidth: number = padding;
 		let lines: Array<string> = [""];
+
 		paragraph.split(" ").forEach((word, widx) => {
 			let wordWidth: number = font.widthOfTextAtSize(word + " ", fontSize);
 			if(wordWidth + currWidth >= width - padding){
@@ -24,10 +31,20 @@ function pageSet(content: string, width: number, padding: number, font: any, fon
 			lines[lines.length - 1] += word + " ";
 			currWidth += wordWidth;
 		});
-		ret += lines.join("\n") + "\n\n";
+
+		lines.forEach((line, lidx) => {
+			if(currHeight + lineHeight >= height - 2 * hPadding){
+				pages.push("");
+				currHeight = hPadding;
+			}
+			pages[pages.length - 1] += line + "\n";
+			currHeight += lineHeight;
+		});
+		pages[pages.length - 1] += "\n";
+		currHeight += lineHeight;
 	});
 
-	return ret;
+	return pages;
 }
 
 async function toPdf(content: string): Promise<void>{
@@ -35,15 +52,17 @@ async function toPdf(content: string): Promise<void>{
 
 	const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
 	const fontSize = 11;
+	const [width, height] = [595.28, 841.89];
 
-	const page = pdfDoc.addPage();
-	const {width, height} = page.getSize();
-
-	page.drawText(pageSet(content, width, 50, font, fontSize), {
-		x: 50,
-		y: height - 4 * fontSize,
-		size: fontSize,
-		font: font
+	let pages: Array<string> = pageSet(content, width, height, 50, font, fontSize);
+	pages.forEach((pageText, idx) => {
+		let page = pdfDoc.addPage();
+		page.drawText(pageText, {
+			x: 50,
+			y: height - 4 * fontSize,
+			size: fontSize,
+			font: font
+		});
 	});
 
 	const pdfBytes = await pdfDoc.save();
